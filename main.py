c521cd02cc8b99e9554b7607f8506eff16f16866
Basil Issac <basilissac@live.com>
2020-03-14 16:11:36 -0500

    Added single link

--- main.py
+++ main.py
@@ -2,28 +2,39 @@
 #TODO: create readme
 from packages import data
 from packages import clustering
+from packages import hierarchical
 import numpy as np
+import scipy
+import sklearn
+import pandas as pd
+from tabulate import tabulate
 
 delim = '^~'
 sourcePath = './data/raw_data.csv'
 tokenPath = './data/tokens.csv'
 nmfPath = './data/nmf.pickle'
 tfidfPath = './data/tfidf_matrix.pickle'
+tfidfPath_small = './data/tfidf_matrix_small.pickle'
 vectorPath = './data/tfidf_vector.pickle'
 kPicklePath = './data/kmeans.pickle'
 featurePickelPath = './data/features.pickle'
 svdPicklePath = './data/svd.pickle'
 num_topics = 10
 
-data.cleanFile(sourcePath,tokenPath,delim)
-data.readFileCreateTFIDF(tokenPath,tfidfPath,vectorPath,featurePickelPath,delim)
-tfidf_matrix = data.readPickle(tfidfPath)
-feat_array = data.readPickle(featurePickelPath)
-svd = data.LoadSVD(tfidf_matrix,feat_array,svdPicklePath,40)
-#data.showSVDPlot(tfidf_matrix)
-model = data.extractTopics(tfidf_matrix,nmfPath,num_topics)
-data.display_topics(model,feat_array,num_topics)
+#data.cleanFile(sourcePath,destPath,delim)
+#data.readFileCreateTFIDF(destPath,tokenPath,tfidfPath,vectorPath,featurePickelPath,delim)
+#tfidf_matrix = data.readPickle(tfidfPath)
+#M,svd = data.ApplySVD(tfidf_matrix,7)
+#feat_array = data.readPickle(featurePickelPath)
+#model = data.extractTopics(tfidf_matrix,nmfPath,num_topics)
+#data.display_topics(model,feat_array,num_topics)
 #kPicklePath = './data/kmeans_svd.pickle'
-#clustering.kmeans(svd,kPicklePath,5)
+#Clustering.kmeans(svd,kPicklePath,5)
+#tfidf_matrix_small = data.readPickle(tfidfPath_small)
+svd_df = pd.DataFrame(pd.read_pickle(svdPicklePath))
+print(svd_df.shape)
+print(svd_df.head())
+hierarchical.singleLink(svd_df.sample(n=int(svd_df.shape[0] * 0.8)))
+
 
 
--- packages/hierarchical.py
+++ packages/hierarchical.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+from sklearn.cluster import AgglomerativeClustering
+import scipy.cluster.hierarchy as shc
+import matplotlib.pyplot as plt
+import time
+import pickle
+from packages import data
+
+def singleLink(tfidf_matrix):
+    start_time = time.time()
+    data = tfidf_matrix.values
+
+    cluster = AgglomerativeClustering(n_clusters=5, affinity='euclidean', linkage='single')
+    cluster.fit_predict(tfidf_matrix)
+    plt.scatter(tfidf_matrix.iloc[:,0],tfidf_matrix.iloc[:,1], c=cluster.labels_, cmap='rainbow')
+    plt.show()
+
+    #plt.title("Dendograms")
+    #dend = shc.dendrogram(shc.linkage(data, method='single'))
+    #plt.show()
+
+    print("--- %s minutes ---" % ((time.time() - start_time)/60))
