15e4f72fb33bffe2a67793912f225190f07d0f8b
Penrod, Jacob <JPENROD1@mail.depaul.edu>
2020-03-16 19:20:55 -0500

    Added command line arguments and generated smaller requirements.txt

--- main.py
+++ main.py
@@ -6,6 +6,27 @@ from packages import hierarchical
 import numpy as np
 import pandas as pd
 from packages import simCluster
+import sys, getopt
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("m",type=str,default='m',help="This selects the memory level for the program the choices are h,m,l for high, medium or low example: py main.py l")
+args = parser.parse_args()
+low_memory = 10000
+medium_memory = 25000
+high_memory = 50000
+#default
+opt = args.m
+hierarchical_clustering_memory = medium_memory
+if opt == 'l':
+    print('Running in Low Memory Mode')
+    hierarchical_clustering_memory = low_memory
+elif opt == 'm':
+    print('Running in Medium Memory Mode')
+    hierarchical_clustering_memory = medium_memory
+elif opt == 'h':
+    print('Running in High Memory Mode')
+    hierarchical_clustering_memory = high_memory 
 delim = '^~'
 sourcePath = './data/raw_data.csv'
 tokenPath = './data/tokens.csv'
@@ -21,11 +42,8 @@ svdPicklePath = './data/svd.pickle'
 simPicklePath = './data/sim.pickle'
 num_topics = 10
 
-low_memory = 10000
-medium_memory = 25000
-high_memory = 50000
 
-hierarchical_clustering_memory = medium_memory
+    
 hierarchical_clustering_number_clusters = 5
 
 data.cleanFile(sourcePath,tokenPath,delim)
@@ -33,7 +51,6 @@ data.readFileCreateTFIDF(tokenPath,tfidfPath,vectorPath,featurePickelPath,delim)
 tfidf_matrix = data.readPickle(tfidfPath)
 feat_array = data.readPickle(featurePickelPath)
 svd = data.LoadSVD(tfidf_matrix,feat_array,svdPicklePath,40)
-#data.showSVDPlot(tfidf_matrix)
 model = data.extractTopics(tfidf_matrix,nmfPath,num_topics)
 data.display_topics(model,feat_array,num_topics)
 
@@ -41,4 +58,3 @@ kPicklePath = './data/kmeans_svd.pickle'
 clustering.kmeans(svd,kPicklePath,5)
 hierarchical.cluster(pd.DataFrame(pd.read_pickle(svdPicklePath)).sample(n=hierarchical_clustering_memory), hierarchical_clustering_number_clusters)
 simCluster.singlepass(svd[:10000],0.5,0)
-#simCluster.singlepass(svd,0.5,0)
